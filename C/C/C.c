44a8f9fafbe81112060126074c78f40764feca45
UserNameIsAlredyTaken <gdaniltatarinof@gmail.com>
2017-10-04 17:59:04 +0300

    Долизал код, написал отчёт

--- C/C/C.c
+++ C/C/C.c
@@ -1,24 +1,21 @@
 #include "stdafx.h"
 #include <stdio.h>
 
-
 const int array1[] = {1,3,5,7,9};
 const int array2[] = {0,2,4,6,8};
 
-int array_sum(const int * first_array, const int * second_array, size_t count) {
-	size_t i;
+int array_sum(const int * first_array, const int * second_array, const size_t count) {	
 	int sum = 0;
-	for (i = 0; i < count; i++) {
+	for (size_t i = 0; i < count; i++) {
 		sum = sum + first_array[i]*second_array[i];
 	}
 	return sum;
 }
 
-int is_prime(unsigned long n) {
+int is_prime(const unsigned long n) {
 	int result = 1;
-	int i;
-	for (i = 2; i<n/2; i++) {
-		if (n%i) {}else{
+	for (int i = 2; i<n/2; i++) {
+		if (!n%i){
 			result = 0;
 			break;
 		}
@@ -28,14 +25,12 @@ int is_prime(unsigned long n) {
 
 int main( int agc, char** argv) {
 	unsigned long input;
-	int right_input;
 	printf("Give me the number!\n");
-	right_input=scanf_s("%lu", &input);
+	int right_input=scanf_s("%lu", &input);
 	if(right_input){
 		if (is_prime(input)){
 			printf("The number %d is prime\n", input);
-		}
-		else {
+		}else {
 			printf("The number %d is not prime\n", input);
 		}
 	}
@@ -43,6 +38,6 @@ int main( int agc, char** argv) {
 		printf("wrong input\n");
 	}
 
-	printf("The sum is: %d\n", array_sum(array1,array2,sizeof(array1) / sizeof(int)));
+	printf("The scalar product is: %d\n", array_sum(array1,array2,sizeof(array1)/sizeof(int)));
 	return 0;
 }
